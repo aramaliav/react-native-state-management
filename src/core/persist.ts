af053d3e1fd3aa591f405b9a8efdff6c7833355c
Liav Arama <aramaliav@gmail.com>
2025-06-01 16:03:16 +0300

    changes

--- src/core/persist.ts
+++ src/core/persist.ts
@@ -1,14 +1,16 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
+import { PersistOption } from '../interfaces/store-interfaces';
 
-export const createPersist = <T>(key: string) => ({
-  save: async (state: T) => {
+export const createAsyncStoragePersist = <T>(key: string): PersistOption<T> => ({
+  save: async (state) => {
     try {
       await AsyncStorage.setItem(key, JSON.stringify(state));
     } catch (e) {
       console.warn('Failed to persist state', e);
     }
   },
-  load: async (): Promise<T | null> => {
+  load: async () => {
     try {
       const json = await AsyncStorage.getItem(key);
       return json ? JSON.parse(json) : null;
@@ -20,6 +22,31 @@ export const createPersist = <T>(key: string) => ({
   clear: async () => {
     try {
       await AsyncStorage.removeItem(key);
+    } catch (e) {
+      console.warn('Failed to clear persisted state', e);
+    }
+  }
+});
+export const createSecureStorePersist = <T>(key: string): PersistOption<T> => ({
+  save: async (state) => {
+    try {
+      await SecureStore.setItemAsync(key, JSON.stringify(state));
+    } catch (e) {
+      console.warn('Failed to persist state', e);
+    }
+  },
+  load: async () => {
+    try {
+      const json = await SecureStore.getItemAsync(key);
+      return json ? JSON.parse(json) : null;
+    } catch (e) {
+      console.warn('Failed to load persisted state', e);
+      return null;
+    }
+  },
+  clear: async () => {
+    try {
+      await SecureStore.deleteItemAsync(key);
     } catch (e) {
       console.warn('Failed to clear persisted state', e);
     }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,22 @@
-import { createPersist } from './core/persist';
 export {
-  useEntities,
-  useEntityById,
-  useActiveEntity,
-  useEntityProperty
+  useActiveEntity, useEntities,
+  useEntityById, useEntityProperty
 } from './hooks/storeHooks';
 
 export {
-  registerStore,
-  getStore,
+  getStore, registerStore
 } from './core/storeRegistry';
 
-export { createPersist } from './core/persist';
+export { createAsyncStoragePersist, createSecureStorePersist } from './core/persist';
 
 export { createEntityStore } from './core/createEntityStore';
 
 export {
-  addEntity,
-  updateEntity,
-  removeEntity,
-  setActiveEntity
+  addEntity, removeEntity,
+  setActiveEntity, updateEntity
 } from './services/generic.service';
 
 export { getAvailableStoreKeys } from './core/storeRegistry';
 
 export type { StoreKey } from './stores/generic.store';
+
